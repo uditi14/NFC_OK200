cf475ae0ea45f12aec56ddd0925b3a0d0ca711dd
uditi14 <uditidt@gmail.com>
2023-09-16 00:19:41 +0530

    card added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login/Login";
 import Reg from "./Components/Register/Reg";
 import Gmap from './Components/Gmap/Gmap'
+import CarCard from "./Components/CarCard/CarCard";
 function App() {
   return (
     <>
@@ -12,6 +12,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Reg />} />{" "}
           <Route path='/map' element={<Gmap/>} />
+          <Route path="/card" element={<CarCard/>}/>
         </Routes>
       </Router>
     </>
--- client/src/Components/Gmap/Gmap.jsx
+++ client/src/Components/Gmap/Gmap.jsx
@@ -1,20 +1,67 @@
-import React, {useCallback,useEffect,useState } from 'react'
-import { GoogleMap,useLoadScript,Marker,Autocomplete} from '@react-google-maps/api'
-import '../../App.css'
+import React, {useEffect,useState } from 'react'
+import { GoogleMap,TrafficLayer,useLoadScript,DirectionsService,DirectionsRenderer} from '@react-google-maps/api'
 
+const libraries = ['places'];
 export default function Gmap() {
-  const {isLoaded} = useLoadScript({
-    googleMapsApiKey:'AIzaSyBfGckgSJfuIJSjlqh02W1KFs6l4DqR7Sk',
+  const {isLoaded}=useLoadScript({
+    googleMapsApiKey:"AIzaSyBfGckgSJfuIJSjlqh02W1KFs6l4DqR7Sk",
+    libraries:libraries,
   })
 
-  const [userLocation,setUserLocation]=useState({lat:19.0759837,lng:72.8776559})
+  const [userLocation, setUserLocation] = useState({lat:19.0759837,lng:72.8776559});
+  const [directions, setDirections] = useState(null);
+
+  const source = { lat: 19.0759837, lng: 72.8776559 };
+  const destination = { lat: 	19.211453, lng: 72.873722};
+
+  useEffect(() => {
+    if (isLoaded) {
+      const directionsService = new window.google.maps.DirectionsService();
+
+      directionsService.route(
+        {
+          origin: source,
+          destination: destination,
+          travelMode: window.google.maps.TravelMode.DRIVING,
+          provideRouteAlternatives: true, // Request alternative routes
+          avoidTolls: false,
+          avoidHighways: false,
+          optimizeWaypoints: true,
+          drivingOptions: {
+            departureTime: new Date(), // Use current time for real-time traffic
+            trafficModel: 'bestguess', // Use "bestguess" to consider traffic congestion
+          },
+        },
+        (result, status) => {
+          if (status === window.google.maps.DirectionsStatus.OK) {
+            setDirections(result);
+          } else {
+            console.error(`Directions request failed due to ${status}`);
+          }
+        }
+      );
+    }
+  }, [isLoaded, source, destination]);
 
   return (
-    <>
-    {isLoaded?(<div>
-        <GoogleMap zoom={15} center={userLocation} mapContainerClassName='map-container'>
-        </GoogleMap>
-    </div>):(<div>Loading</div>)}
-    </>
+    <div>
+      {isLoaded?(
+        <div>
+          <GoogleMap zoom={15} center={userLocation} mapContainerClassName='map-container'>
+            <TrafficLayer></TrafficLayer>
+            {directions && (
+              <DirectionsRenderer
+                directions={directions}
+                options={{
+                  polylineOptions: {
+                    strokeColor: 'blue', 
+                  },
+                }}
+              />
+            )}
+          </GoogleMap>
+        </div>
+      ):(<div>Loading</div>)}
+    </div>
   )
 }
